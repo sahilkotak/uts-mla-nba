415298374107c3f8a552c516522404ef9458d761
sahil-profitice <sahil@profitice.com>
2023-08-31 04:46:22 +0000

    - feature engineering
    Noticed a downgrade in performance, dropping this experiment route.

--- uts-mla-nba/src/models/svm_logistic_ensemble_v2.py
+++ uts-mla-nba/src/models/svm_logistic_ensemble_v2.py
@@ -4,11 +4,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 import numpy as np
 import pandas as pd
-
 
 def train_stacking_ensemble(train_dataframe, target_column_name, test_size=0.2, random_state=42):
     """
@@ -32,17 +30,14 @@ def train_stacking_ensemble(train_dataframe, target_column_name, test_size=0.2, 
         features, target, test_size=test_size, random_state=random_state)
 
     # Identify numerical and categorical columns
-    numerical_columns = features.select_dtypes(
-        include=['int64', 'float64']).columns.tolist()
-    categorical_columns = features.select_dtypes(
-        include=['object']).columns.tolist()
+    numerical_columns = features.select_dtypes(include=['int64', 'float64']).columns.tolist()
+    categorical_columns = features.select_dtypes(include=['object']).columns.tolist()
 
     # Create a preprocessor
     preprocessor = ColumnTransformer(
         transformers=[
             ('numeric', StandardScaler(), numerical_columns),
-            ('categorical', OneHotEncoder(drop='first',
-             handle_unknown='ignore'), categorical_columns)
+            ('categorical', OneHotEncoder(drop='first', handle_unknown='ignore'), categorical_columns)
         ])
 
     # Create pipelines for both logistic regression and SVM
@@ -61,10 +56,8 @@ def train_stacking_ensemble(train_dataframe, target_column_name, test_size=0.2, 
     svm_pipeline.fit(features_train, target_train)
 
     # Use trained models to generate predictions on the validation set
-    logreg_probabilities = logreg_pipeline.predict_proba(features_val)[
-        :, 1].reshape(-1, 1)
-    svm_probabilities = svm_pipeline.predict_proba(features_val)[
-        :, 1].reshape(-1, 1)
+    logreg_probabilities = logreg_pipeline.predict_proba(features_val)[:, 1].reshape(-1, 1)
+    svm_probabilities = svm_pipeline.predict_proba(features_val)[:, 1].reshape(-1, 1)
 
     # Stack the predictions to create new features for the meta-model
     stacked_features_val = np.hstack([logreg_probabilities, svm_probabilities])
@@ -74,8 +67,7 @@ def train_stacking_ensemble(train_dataframe, target_column_name, test_size=0.2, 
     meta_model.fit(stacked_features_val, target_val)
 
     # Predict probabilities on the validation set using the meta-model
-    ensemble_probabilities = meta_model.predict_proba(stacked_features_val)[
-        :, 1]
+    ensemble_probabilities = meta_model.predict_proba(stacked_features_val)[:, 1]
 
     # Compute AUROC score
     auroc_score = roc_auc_score(target_val, ensemble_probabilities)
